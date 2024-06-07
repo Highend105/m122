3cb458e629df811dcf79966d832b809bdc0e1c42
Lukas <142900064+Highend105@users.noreply.github.com>
2024-06-07 13:43:52 +0200

    Script1

---
+++ Auftrag-1/erstelleVorlagen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p _templates
+touch _templates/datei-1.txt
+touch _templates/datei-2.pdf
+touch _templates/datei-3.doc
+mkdir -p _schulklassen
+touch _schulklassen/M122-AP22b.txt
+touch _schulklassen/M122-AP22c.txt
+touch _schulklassen/M122-AP22d.txt
+echo -e "Müller\nSchmidt\nTrinkler\nDuss\nIneichen\nFernandes\nHeinzer\nGates\nMusk\nShaqiri\nXhaka\nRonaldo" > _schulklassen/M122-AP22b.txt
+echo -e "Müller\nSchmidt\nTrinkler\nDuss\nIneichen\nFernandes\nHeinzer\nGates\nMusk\nShaqiri\nXhaka\nRonaldo" > _schulklassen/M122-AP22c.txt
+echo -e "Müller\nSchmidt\nTrinkler\nDuss\nIneichen\nFernandes\nHeinzer\nGates\nMusk\nShaqiri\nXhaka\nRonaldo" > _schulklassen/M122-AP22d.txt
+
