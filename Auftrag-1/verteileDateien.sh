42d1d7821b36229cf03012d7e5858083a8dbcfc0
Lukas <142900064+Highend105@users.noreply.github.com>
2024-06-14 14:42:46 +0200

    Update verteileDateien.sh

--- Auftrag-1/verteileDateien.sh
+++ Auftrag-1/verteileDateien.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 for Klasse_Datei in _schulklassen/*.txt; do
-Klasse_Ordner=$(basename "$Klasse_Datei" .txt)
-
-mkdir -p "$Klasse_Ordner"
-
-while IFS= read -r schueler; do
-schueler_ordner="$Klasse_Ordner/$schueler"
-
-mkdir -p "$schueler_ordner"
-
-cp _templates/* "$Klasse_Ordner/"
+    Klasse_Ordner=$(basename "$Klasse_Datei" .txt)
+    mkdir -p "$Klasse_Ordner"
+    while IFS= read -r schueler; do
+       schueler_ordner="$Klasse_Ordner/$schueler"
+       mkdir -p "$schueler_ordner"
+       cp _templates/* "$Klasse_Ordner/"
     done < "$Klasse_Datei"
 done
